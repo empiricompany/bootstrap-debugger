a8596c086eacec2799c093adefad306aa7576081
empiricompany <tonyweboss@gmail.com>
2014-12-23 15:14:31 +0100

    new feature grid overlay
    Add live grid overlay toggle and gutter with info

--- bootstrap-debugger.js
+++ bootstrap-debugger.js
@@ -1,5 +1,6 @@
 var currentBootstrapBreakPoint;	
 var version;
+var gutter;
 (function( $ ) {
 	
 	
@@ -39,7 +40,15 @@ var version;
 						 $('.container').width() + 'px' +	
 					'</td>' +							
 				'</tr>' +
-				'<tr>';
+				'<tr>' +
+					'<td style="'+styleLabel+'">' +				
+						'gutter' +
+					'</td>' +	
+					'<td style="'+styleValue+'">' +							
+						 gutter +	
+					'</td>' +							
+				'</tr>' +
+				'</tr>';
 			}
 			infoHtml+= '<td style="'+styleLabel+'">' +					
 					'width:' +
@@ -64,11 +73,24 @@ var version;
 					 $(window).scrollTop() + 'px' +
 				'</td>' +													
 			'</tr>' +
+			'<tr><td colspan="2" style="font-size:14px;text-align: center; color: white;font-weight: bold; font-family: monospace; cursor:pointer" class="toggleGrid">' +
+				'toggleGrid()' +
+			'</td></tr>' +
 		'</table>' + 													
-		'<div class="move moveBottom" style="text-align:center;;cursor:pointer;color:red;">▼</div>' +								
+		'<div class="move moveBottom" style="text-align:center;cursor:pointer;color:red;">▼</div>' +								
 		'</div>';	
 		$('.mode-informer').html(infoHtml);
+
+		if($('#grid-debugger').is(':visible')){
+			$('.toggleGrid').css({color: '#00ff00'});
+		}else{
+			$('.toggleGrid').css({color: 'white'});
+		}
 		
+		$('.toggleGrid').off('click').on('click', function(){
+			toggleGrid();
+		});
+
 		$('.moveLeft').off('click').on('click', function(){
 			$('.mode-informer').css({
 					left: 10,
@@ -111,6 +133,10 @@ var version;
 				var env = envs[i];
 				$el.addClass('hidden-'+env);
 				if ($el.is(':hidden')) {
+					$elGutter = $('<div>');
+					$elGutter.addClass('col-'+env+'-1');
+					gutter = $elGutter.css('padding-left');
+					$elGutter.remove();
 					$el.remove();
 					try {
 						console.log(env);
@@ -136,6 +162,9 @@ var version;
 				var env = envs[i];
 				$el.addClass('hidden-'+env);
 				if ($el.is(':hidden')) {
+					$elGutter = $('<div>');
+					$elGutter.addClass('span1');
+					gutter = $elGutter.css('padding-left');
 					$el.remove();
 					try {
 						console.log(env);
@@ -155,6 +184,78 @@ var version;
 		return;
 	}
 
+	function gridCreate(){
+		//$('#grid-debugger').remove();
+
+		$('<div />', {id: "grid-debugger"}).css({
+			'display': 'none',
+			'height': '100%',
+			'width': '100%',
+		    'box-sizing': 'padding-box',
+		    'height': '100%',
+		    'left': 0,
+		    'pointer-events': 'none',
+		    'position': 'fixed',
+		    'top': 0,
+		    'z-index': 9999
+		}).prependTo('body');
+
+		$('<div />', { class: "container" }).css({height: '100%'})
+		.appendTo('#grid-debugger');
+
+		$('<div />', { class: "row" }).css({height: '100%'}).appendTo('#grid-debugger .container');
+
+		if(version = '3.x'){
+			classColumn = 'col-xs-1 col-sm-1 col-md-1 col-lg-1';
+			htmlColumn = '<div class="visible-xs">xs</div><div class="visible-sm">sm</div><div class="visible-md">md</div><div class="visible-lg">lg</div>';
+		}else{
+			classColumn = 'span1';
+			htmlColumn = '<div class="visible-phone">phone</div><div class="visible-tablet">tablet</div><div class="visible-desktop">desktop</div>';
+		}
+
+		for (i = 12; i >= 1; i--) { 
+		  $('<div />', { class: classColumn, id: 'checkWidth'+i})
+		    .css({
+		    	'height': '100%',
+		      	'background-color': 'black',
+			    'border-color': 'white',
+			    'border-style': 'solid',
+			    'opacity': '0.1',
+			    'outline': '1px solid red',
+			    'text-align': 'center',
+			    'font-size': '18px',
+			    'font-weight': 'bold',
+			    'font-family': 'monospace',
+			    'color': 'white'
+			})
+		  .html(htmlColumn+' <div class="infoWidth"></div>')
+		  .prependTo('#grid-debugger .container .row');
+		}
+		//Adjust gutter
+		$('#grid-debugger .container .row > div').css({
+			'padding': 0,
+			'border-width': '0 '+gutter
+		}).hover(function(){
+			$(this).css('opacity', '0.75');
+		},function(){
+			$(this).css('opacity', '0.1');
+		})
+		//console.log($('#checkWidth1').width());
+		//$('#grid-debugger .container .row div.infoWidth').html( $('#checkWidth1').width() + 'gutter '+gutter );
+		//$('#grid-debugger .container .row div.infoWidth').html( 'gutter '+gutter );
+	}
+
+	function toggleGrid(){
+		$('#grid-debugger').toggle('fade', function(){
+			if($(this).is(':visible')){
+				$('.toggleGrid').css({color: '#00ff00'});
+			}else{
+				$('.toggleGrid').css({color: 'white'});
+			}
+		});
+	}
+
+
 	$(window).resize(function(e){
 		findBootstrapBreakPoints(e);
 	});
@@ -162,12 +263,15 @@ var version;
 		findBootstrapBreakPoints(e);
 	});
 	$(document).ready(function(e){
-		$('<div class="mode-informer" style="z-index:99999; opacity:0.75;background:#000;color:#fff;padding:10px;border-radius:4px;position: fixed;bottom:10px;right:10px">%mode%</div>').appendTo('body').hover(function(){
+		$('<div class="mode-informer" style="z-index:99999; opacity:0.75;background:#000;color:#fff;padding:10px 20px;border-radius:4px;position: fixed;bottom:10px;right:10px">%mode%</div>').appendTo('body').hover(function(){
 			$(this).css('opacity', 1);
 		},function(){
 			$(this).css('opacity', 0.75);			
 		});
+
 		findBootstrapBreakPoints(e);
+		gridCreate();
 	});	
+
 	
 }( jQuery ));
